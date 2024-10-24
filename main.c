fce88edc9053dc1c8580bc8492786c1ab37f9287
Ahmed  Eltokhey <139669976+ahmdeltoky03@users.noreply.github.com>
2024-10-24 08:02:52 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include "school.h"
 
 int main() {
-    /* evevy user have only three attempts*/
+    // number of attempt = 5
     u32 attempt = 1;
-    u32 Max_Number_of_Attempts = 3;//number of times that allowed to evevy user
+    u32 Max_Number_of_Attempts = 5;
     u32 numberStudents = 0;
-    u32 Number_of_attempts = Max_Number_of_Attempts;//represent Number of times allowed to enter
+    u32 Number_of_attempts = Max_Number_of_Attempts;
     u8 password[]="ahmed";
-    u8 userpassword [20];
+    u8 userpassword [40];
     struct Node* studentsList = NULL;
 
     while (Number_of_attempts > 0) {
